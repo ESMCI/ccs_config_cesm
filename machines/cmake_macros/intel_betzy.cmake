65c520e7dd6c289e294307310f4f754c1a31c89d
mvdebolskiy <80036033+mvdebolskiy@users.noreply.github.com>
2023-03-28 20:25:09 +0200

    Added betzy

---
+++ machines/cmake_macros/intel_betzy.cmake
@@ -0,0 +1,21 @@
+
+#set(MPICC "mpiicc")
+#set(MPICXX "mpiicpc")
+#set(MPIFC "mpiifort")
+
+set(SCC "icc")
+set(SCXX "icpc")
+set(SFC "ifort")
+
+set(PIO_FILESYSTEM_HINTS "lustre")
+
+set(NETCDF_C_PATH	$ENV{EBROOTNETCDF})
+set(NETCDF_FORTRAN_PATH $ENV{EBROOTNETCDFMINFORTRAN})
+set(NETCDF		$ENV{EBROOTNETCDFMINFORTRAN})
+set(NETCDF_PATH		$ENV{EBROOTNETCDF})
+set(PNETCDF_PATH	$ENV{PNETCDF})
+
+if (COMP_NAME STREQUAL blom)
+   string(APPEND FFLAGS FC_AUTO_R8)
+endif()
+
