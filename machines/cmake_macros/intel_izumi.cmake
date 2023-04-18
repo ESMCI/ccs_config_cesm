dc66fd06256d6eb5e6fa493042123b4b54302e81
Bill Sacks <sacks@ucar.edu>
2023-04-18 16:59:18 -0600

    Revert "Add subdirectory in PFUNIT_PATH"
    This reverts commit 8a7b4c61c6ace2a4905bf6aff973a28509e63a93.
    
    This seems not to be needed after all (I mis-diagnosed the issue)

--- machines/cmake_macros/intel_izumi.cmake
+++ machines/cmake_macros/intel_izumi.cmake
@@ -1,3 +1,3 @@
 if (MPILIB STREQUAL mpi-serial AND NOT compile_threaded)
-  set(PFUNIT_PATH "/fs/cgd/csm/tools/pFUnit/pFUnit4.7.0_izumi_Intel20.0.1_noMPI_noOpenMP/PFUNIT-4.7/cmake")
+  set(PFUNIT_PATH "/fs/cgd/csm/tools/pFUnit/pFUnit4.7.0_izumi_Intel20.0.1_noMPI_noOpenMP")
 endif()
