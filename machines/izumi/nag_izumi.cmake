8c98faa7d9483c582f59f35fc10ae2da3c238b3f
Jim Edwards <jedwards@ucar.edu>
2025-05-08 12:10:55 -0600

    update izumi to use nag7

--- machines/izumi/nag_izumi.cmake
+++ machines/izumi/nag_izumi.cmake
@@ -1,1 +1,2 @@
 string(APPEND SLIBS " -L${NETCDF_PATH}/lib -lnetcdff -lnetcdf -Wl,-Wl,,-rpath,$(NETCDF_PATH)/lib")
+string(APPEND CONFIG_ARGS " FCLIBS='-Wl,--as-needed,--allow-shlib-undefined  -L$(COMPILER_PATH)/lib/NAG_Fortran -lf70rts'")
