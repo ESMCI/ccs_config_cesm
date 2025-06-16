64487b815fa9bcdfd8b9d65211564d0fe9a1e1b5
Jim Edwards <jedwards@ucar.edu>
2025-06-16 14:51:01 -0600

    clean up and change max mpi tasks to 24

--- machines/izumi/gnu_izumi.cmake
+++ machines/izumi/gnu_izumi.cmake
@@ -1,2 +1,1 @@
-string(APPEND FFLAGS " -fallow-argument-mismatch  -fallow-invalid-boz ")
-string(APPEND LDFLAGS " -Wl,-rpath,$LD_LIBRARY_PATH")
+string(APPEND FFLAGS " -fallow-argument-mismatch -fallow-invalid-boz ")
